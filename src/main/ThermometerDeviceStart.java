41ed6aa81bff6fc8e25901b0d8c6f7fccc5f3aae
hviezca <hviezca@my.gcu.edu>
2021-10-27 20:31:31 -0600

    Milestone 4 Logger and Artifacts Upate

--- src/main/ThermometerDeviceStart.java
+++ src/main/ThermometerDeviceStart.java
@@ -7,9 +7,9 @@ public class ThermometerDeviceStart {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
-		String device_name = "Meat Probe";
-		String username ="shawn";
-		String password ="1234";
+		String device_name = "Temp Master";
+		String username ="kronos";
+		String password ="123asdf";
 		String restURL = "http://localhost:8080/ThermometerProject/rest/rest/posttemps";
 		
 		ThermometerDevice thermo = new ThermometerDevice(device_name, username, password, restURL);
