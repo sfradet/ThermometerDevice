f89378c63c2223b8d6e59f99001290dc9393a104
sfradet <sfradet@hotmail.com>
2021-10-29 19:32:57 -0700

    Update ThermometerDeviceStart.java

--- src/main/ThermometerDeviceStart.java
+++ src/main/ThermometerDeviceStart.java
@@ -7,9 +7,9 @@ public class ThermometerDeviceStart {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
-		String device_name = "Temp Master";
-		String username ="kronos";
-		String password ="123asdf";
+		String device_name = "Device Name";
+		String username ="username";
+		String password ="password";
 		String restURL = "http://localhost:8080/ThermometerProject/rest/rest/posttemps";
 		
 		ThermometerDevice thermo = new ThermometerDevice(device_name, username, password, restURL);
